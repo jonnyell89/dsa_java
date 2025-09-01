44960a428cb34e21a1b52b2628db3eb5087bd4f3
jonnyell89 <jonnyell89@gmail.com>
2025-09-01 16:35:25 +0100

    ManualArray class iterator method tests rewritten to use lambda functions instead of anonymous classes

--- src/test/java/org/example/collections/ManualArrayTest.java
+++ src/test/java/org/example/collections/ManualArrayTest.java
@@ -404,6 +404,14 @@ public class ManualArrayTest {
     }
 
     @Test
+    void testManualArrayForEachIteratorLambda() {
+        ManualArray manualArrayForInsertion = new ManualArray();
+        ManualArray manualArrayForTraversal = new ManualArray(array);
+        manualArrayForTraversal.forEach(element -> manualArrayForInsertion.insertAtEnd(element));
+        assertArrayEquals(manualArrayForTraversal.toArray(), manualArrayForInsertion.toArray());
+    }
+
+    @Test
     void testManualArrayFilterIterator() {
         ManualArray manualArray = new ManualArray(array);
         ManualArray result = manualArray.filter(new IntPredicate() {
@@ -417,6 +425,14 @@ public class ManualArrayTest {
     }
 
     @Test
+    void testManualArrayFilterIteratorLambda() {
+        ManualArray manualArray = new ManualArray(array);
+        ManualArray result = manualArray.filter(element -> element % 2 == 0);
+        int[] arrayWithEvenElements = new int[]{80, 202};
+        assertArrayEquals(arrayWithEvenElements, result.toArray());
+    }
+
+    @Test
     void testManualArrayMapIterator() {
         ManualArray manualArray = new ManualArray(array);
         ManualArray result = manualArray.map(new IntUnaryOperator() {
@@ -425,6 +441,14 @@ public class ManualArrayTest {
                 return element * 2;
             }
         });
+        int[] arrayWithDoubledElements = new int[]{6, 34, 150, 160, 404};
+        assertArrayEquals(arrayWithDoubledElements, result.toArray(), "ManualArray result should be equal to arrayWithDoubledElements.");
+    }
+
+    @Test
+    void testManualArrayMapIteratorLambda() {
+        ManualArray manualArray = new ManualArray(array);
+        ManualArray result = manualArray.map(element -> element * 2);
         int[] arrayWithDoubledElements = new int[]{6, 34, 150, 160, 404};
         assertArrayEquals(arrayWithDoubledElements, result.toArray(), "ManualArray result should be equal to arrayWithDoubledElements.");
     }
@@ -443,6 +467,14 @@ public class ManualArrayTest {
     }
 
     @Test
+    void testManualArrayReduceIteratorLambda() {
+        ManualArray manualArray = new ManualArray(array);
+        int result = manualArray.reduce((accumulator, element) -> accumulator + element, 0);
+        int sumOfArrayElements = 377;
+        assertEquals(sumOfArrayElements, result, "ManualArray result should be equal to sumOfArrayElements.");
+    }
+
+    @Test
     void testManualArraySomeIterator() {
         ManualArray manualArray = new ManualArray(array);
         boolean result = manualArray.some(new IntPredicate() {
@@ -455,6 +487,13 @@ public class ManualArrayTest {
     }
 
     @Test
+    void testManualArraySomeIteratorLambda() {
+        ManualArray manualArray = new ManualArray(array);
+        boolean result = manualArray.some(element -> element % 2 != 0);
+        assertTrue(result, "ManualArray should contain one or more odd numbers.");
+    }
+
+    @Test
     void testManualArrayEveryIterator() {
         ManualArray manualArray = new ManualArray(array);
         boolean result = manualArray.every(new IntPredicate() {
@@ -463,6 +502,13 @@ public class ManualArrayTest {
                 return element >= 0;
             }
         });
+        assertTrue(result, "ManualArray should contain all positive numbers.");
+    }
+
+    @Test
+    void testManualArrayEveryIteratorLambda() {
+        ManualArray manualArray = new ManualArray(array);
+        boolean result = manualArray.every(element -> element > 0);
         assertTrue(result, "ManualArray should contain all positive numbers.");
     }
 }
