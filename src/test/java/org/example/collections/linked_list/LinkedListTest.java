12eff948ba385e53dbf15853afb26893341fa3d9
jonnyell89 <jonnyell89@gmail.com>
2025-09-13 19:26:54 +0100

    LinkedListTest class tests added for read method, search method, insertAtStart method, insertAtEnd method

--- src/test/java/org/example/collections/linked_list/LinkedListTest.java
+++ src/test/java/org/example/collections/linked_list/LinkedListTest.java
@@ -15,13 +15,14 @@ public class LinkedListTest {
     @BeforeEach
     void testInit() {
         linkedList = new LinkedList<>();
-        linkedList.insertAtStart(1);
-        linkedList.insertAtStart(2);
-        linkedList.insertAtStart(3);
         linkedList.insertAtStart(4);
-        linkedList.insertAtStart(5);
+        linkedList.insertAtStart(3);
+        linkedList.insertAtStart(2);
+        linkedList.insertAtStart(1);
+        linkedList.insertAtStart(0);
     }
 
+    // READ
     @Test
     void testLinkedListReadWhenEmpty() {
         int index = 0;
@@ -31,15 +32,6 @@ public class LinkedListTest {
                 () -> linkedList.read(index),
                 "LinkedList head is null."
         );
-    }
-
-    @Test
-    void testLinkedListInsertAtStart() {
-        Integer data = 0;
-        linkedList.insertAtStart(data);
-        int index = 0;
-        Integer result = linkedList.read(index);
-        assertEquals(data, result, String.format("Data at index %d should be %d.", index, data));
     }
 
     @Test
@@ -64,8 +56,61 @@ public class LinkedListTest {
 
     @Test
     void testLinkedListReadWithValidIndex() {
-        int index = 4;
+        int size = linkedList.getSize() - 1;
+        for (int i = 0; i < size; i++) {
+            Integer result = linkedList.read(i);
+            assertEquals(i, result, String.format("Data at index %d should be %d.", i, result));
+        }
+    }
+
+    // SEARCH
+    @Test
+    void testLinkedListSearchWhenEmpty() {
+        Integer data = 5;
+        linkedList = new LinkedList<>();
+        NoSuchElementException exception = assertThrows(
+                NoSuchElementException.class,
+                () -> linkedList.search(data),
+                "LinkedList head is null."
+        );
+    }
+
+    @Test
+    void testLinkedListSearchWithInvalidData() {
+        Integer data = 5;
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> linkedList.search(data),
+                "Data is not present in LinkedList."
+        );
+    }
+
+    @Test
+    void testLinkedListSearchWithValidData() {
+        int size = linkedList.getSize() - 1;
+        for (int i = 0; i < size; i++) {
+            int result = linkedList.search(i);
+            assertEquals(i, result, String.format("Data at LinkedList[%d] should return index %d.", i, i));
+        }
+    }
+
+    // INSERT
+    @Test
+    void testLinkedListInsertAtStart() {
+        Integer data = 5;
+        linkedList.insertAtStart(data);
+        int index = 0;
         Integer result = linkedList.read(index);
-        assertEquals(index, result, String.format("Data at index %d should be %d.", index, result));
+        assertEquals(data, result, String.format("Data at index %d should be %d.", index, data));
+    }
+
+    @Test
+    void testLinkedListInsertAtEndWhenEmpty() {
+        Integer data = 5;
+        linkedList = new LinkedList<>();
+        linkedList.insertAtEnd(data);
+        int index = 0;
+        int result = linkedList.read(index);
+        assertEquals(data, result, String.format("Data at index %d should be %d.", index, data));
     }
 }
