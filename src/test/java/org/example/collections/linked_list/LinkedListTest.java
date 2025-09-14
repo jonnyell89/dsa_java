95742d73e26e1c23dc83e86803f2e30ebb01b52b
jonnyell89 <jonnyell89@gmail.com>
2025-09-14 14:49:04 +0100

    LinkedListTest class tests added for insertAtEnd method, insertAtIndex method, deleteFromStart method, deleteFromEnd method, deleteFromIndex method

--- src/test/java/org/example/collections/linked_list/LinkedListTest.java
+++ src/test/java/org/example/collections/linked_list/LinkedListTest.java
@@ -88,9 +88,9 @@ public class LinkedListTest {
     @Test
     void testLinkedListSearchWithValidData() {
         int size = linkedList.getSize() - 1;
-        for (int i = 0; i < size; i++) {
-            int result = linkedList.search(i);
-            assertEquals(i, result, String.format("Data at LinkedList[%d] should return index %d.", i, i));
+        for (int data = 0; data < size; data++) {
+            int result = linkedList.search(data);
+            assertEquals(data, result, String.format("Data at LinkedList[%d] should return index %d.", data, data));
         }
     }
 
@@ -110,7 +110,149 @@ public class LinkedListTest {
         linkedList = new LinkedList<>();
         linkedList.insertAtEnd(data);
         int index = 0;
-        int result = linkedList.read(index);
+        Integer result = linkedList.read(index);
         assertEquals(data, result, String.format("Data at index %d should be %d.", index, data));
+    }
+
+    @Test
+    void testLinkedListInsertAtEnd() {
+        Integer data = 5;
+        linkedList.insertAtEnd(data);
+        int index = linkedList.getSize() - 1;
+        Integer result = linkedList.read(index);
+        assertEquals(data, result, String.format("Data at index %d should be %d.", index, data));
+    }
+
+    @Test
+    void testLinkedListInsertAtIndexWithIndexLessThanZero() {
+        int indexLessThanZero = -5;
+        Integer data = 5;
+        IndexOutOfBoundsException exception = assertThrows(
+                IndexOutOfBoundsException.class,
+                () -> linkedList.insertAtIndex(indexLessThanZero, data),
+                "LinkedList index should not be less than zero."
+        );
+    }
+
+    @Test
+    void testLinkedListInsertAtIndexWithIndexGreaterThanSize() {
+        int indexGreaterThanSize = 15;
+        Integer data = 5;
+        IndexOutOfBoundsException exception = assertThrows(
+                IndexOutOfBoundsException.class,
+                () -> linkedList.insertAtIndex(indexGreaterThanSize, data),
+                "LinkedList index should not be greater than size."
+        );
+    }
+
+    @Test
+    void testLinkedListInsertAtIndexWithIndexZero() {
+        int index = 0;
+        Integer data = 5;
+        linkedList.insertAtIndex(index, data);
+        Integer result = linkedList.read(index);
+        assertEquals(data, result, String.format("Data at index %d should be %d.", index, data));
+    }
+
+    @Test
+    void testLinkedListInsertAtIndexWithValidIndex() {
+        int index = 5;
+        Integer data = 5;
+        linkedList.insertAtIndex(index, data);
+        Integer result = linkedList.read(index);
+        assertEquals(data, result, String.format("Data at index %d should be %d.", index, data));
+    }
+
+    // DELETE
+    @Test
+    void testLinkedListDeleteFromStartWhenEmpty() {
+        linkedList = new LinkedList<>();
+        NoSuchElementException exception = assertThrows(
+                NoSuchElementException.class,
+                () -> linkedList.deleteFromStart(),
+                "Cannot delete from an empty LinkedList."
+        );
+    }
+
+    @Test
+    void testLinkedListDeleteFromStart() {
+        int index = 0;
+        Integer data = linkedList.read(index);
+        Integer result = linkedList.deleteFromStart();
+        assertEquals(data, result, String.format("Data at index %d should be %d.", index, data));
+    }
+
+    @Test
+    void testLinkedListDeleteFromEndWhenEmpty() {
+        linkedList = new LinkedList<>();
+        NoSuchElementException exception = assertThrows(
+                NoSuchElementException.class,
+                () -> linkedList.deleteFromEnd(),
+                "Cannot delete from an empty LinkedList."
+        );
+    }
+
+    @Test
+    void testLinkedListDeleteFromEndWithSizeOne() {
+        linkedList = new LinkedList<>();
+        Integer data = 5;
+        linkedList.insertAtStart(data);
+        Integer result = linkedList.deleteFromEnd();
+        assertEquals(data, result, String.format("Data %d should be equal to result %d.", data, result));
+    }
+
+    @Test
+    void testLinkedListDeleteFromEnd() {
+        int index = linkedList.getSize() - 1;
+        Integer data = linkedList.read(index);
+        Integer result = linkedList.deleteFromEnd();
+        assertEquals(data, result, String.format("Data %d should be equal to result %d.", data, result));
+    }
+
+    @Test
+    void testLinkedListDeleteFromIndexWhenEmpty() {
+        linkedList = new LinkedList<>();
+        int index = 0;
+        NoSuchElementException exception = assertThrows(
+                NoSuchElementException.class,
+                () -> linkedList.deleteFromIndex(index),
+                "Cannot delete from an empty LinkedList."
+        );
+    }
+
+    @Test
+    void testLinkedListDeleteFromIndexWithIndexLessThanZero() {
+        int indexLessThanZero = -5;
+        IndexOutOfBoundsException exception = assertThrows(
+                IndexOutOfBoundsException.class,
+                () -> linkedList.deleteFromIndex(indexLessThanZero),
+                "LinkedList index should not be less than zero."
+        );
+    }
+
+    @Test
+    void testLinkedListDeleteFromIndexWithIndexGreaterThanSize() {
+        int indexGreaterThanSize = 15;
+        IndexOutOfBoundsException exception = assertThrows(
+                IndexOutOfBoundsException.class,
+                () -> linkedList.deleteFromIndex(indexGreaterThanSize),
+                "LinkedList index should not be greater than size."
+        );
+    }
+
+    @Test
+    void testLinkedListDeleteFromIndexWithIndexZero() {
+        int index = 0;
+        Integer data = linkedList.read(index);
+        Integer result = linkedList.deleteFromIndex(index);
+        assertEquals(data, result, String.format("Data %d should be equal to result %d.", data, result));
+    }
+
+    @Test
+    void testLinkedListDeleteFromIndex() {
+        int index = 1;
+        Integer data = linkedList.read(index);
+        Integer result = linkedList.deleteFromIndex(index);
+        assertEquals(data, result, String.format("Data %d should be equal to result %d.", data, result));
     }
 }
