e15850f9c0a5ea3b0c6caa9e639efffc77381b5c
jonnyell89 <jonnyell89@gmail.com>
2025-09-24 15:34:53 +0100

    put method added, get method added, remove method added

--- src/main/java/org/example/collections/MyHashMap.java
+++ src/main/java/org/example/collections/MyHashMap.java
@@ -15,7 +15,9 @@ public class MyHashMap<K, V> {
         }
     }
 
-    private int capacity = 16;
+    private final int DEFAULT_CAPACITY = 16;
+
+    private int capacity = DEFAULT_CAPACITY;
     private float loadFactor = 0.75f;
     private int size;
     private Node<K, V>[] data;
@@ -27,5 +29,67 @@ public class MyHashMap<K, V> {
 
     private int hash(K key) {
         return (key == null) ? 0 : Math.abs(key.hashCode() % capacity);
+    }
+
+    public void put(K key, V value) {
+        int index = hash(key);
+        Node<K, V> head = data[index];
+
+        for (Node<K, V> node = head; node != null; node = node.next) {
+            if (node.key.equals(key)) {
+                node.value = value;
+                return;
+            }
+        }
+        data[index] = new Node<>(key, value, head);
+        size++;
+
+        if ((float)size / capacity >= loadFactor) {
+            ensureCapacity();
+        }
+    }
+
+    public V get(K key) {
+        int index = hash(key);
+
+        for (Node<K, V> node = data[index]; node != null; node = node.next) {
+            if (node.key.equals(key)) return node.value;
+        }
+        return null;
+    }
+
+    public V remove(K key) {
+        int index = hash(key);
+        Node<K, V> head = data[index];
+        Node<K, V> dummy = new Node<>(null, null, head);
+
+        for (Node<K, V> node = dummy; node.next != null; node = node.next) {
+            if (node.next.key.equals(key)) {
+                V value = node.next.value;
+                node.next.key = null;
+                node.next.value = null;
+                node.next = node.next.next;
+                data[index] = dummy.next;
+                size--;
+                return value;
+            }
+        }
+        return null;
+    }
+
+    @SuppressWarnings("unchecked")
+    public void ensureCapacity() {
+        if ((float)size / capacity >= loadFactor) {
+            capacity = capacity * 2;
+            size = 0;
+            Node<K, V>[] oldData = data;
+            data = new Node[capacity];
+
+            for (Node<K, V> head : oldData) {
+                for (Node<K, V> node = head; node != null; node = node.next) {
+                    put(node.key, node.value);
+                }
+            }
+        }
     }
 }
